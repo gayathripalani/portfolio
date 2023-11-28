77d6cdbaa994094cdcc3c28a8243a270afbcbed4
Gayathri Palani <gayathri.palani7@gmail.com>
2023-11-28 20:37:34 +0100

    Update resune

--- src/primitives/Button.js
+++ src/primitives/Button.js
@@ -5,9 +5,10 @@ export const Button = styled.button`
   border-radius: 3px;
   border: 2px solid #0052D4;
   color: #0052D4;
-  margin: 0.5em 1em;
-  padding: 0.25em 1em;
+  margin: 1em 2em;
+  padding: 0.5em 2em;
   font-weight: bold;
+  cursor: pointer;
 
   ${props => props.primary && css`
     background: #0052D4;
